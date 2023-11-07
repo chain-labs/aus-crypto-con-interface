640b7c563ecdc4940586e4b2846b4035e78ff0eb
Angel Lakra <angel.lakra@chainlabs.in>
2023-11-07 23:41:16 +0530

    added sentry

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://c08aa5d441593bf0e09dec9ffffeaaa9@o4506185897476096.ingest.sentry.io/4506185898655744",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
--- src/pages/api/webhook.ts
+++ src/pages/api/webhook.ts
@@ -1,4 +1,5 @@
 import { json } from 'express'
+import * as Sentry from '@sentry/nextjs'
 
 export default function handler(req, res) {
   if (req.method === 'POST') {
@@ -6,6 +7,11 @@ export default function handler(req, res) {
     json()(req, res, () => {
       //   const { name, email } = req.body // Access the request body
       console.log({ req: req.body })
+      Sentry.captureMessage('Found Event', {
+        tags: {
+          body: req.body,
+        },
+      })
 
       // Perform actions (e.g., create a user) here
       res.status(200).json({ message: 'User created successfully' })
